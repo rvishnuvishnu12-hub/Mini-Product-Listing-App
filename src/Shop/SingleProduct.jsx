6550ef0e3560670add6f6bb2577923694b416c0b
vishnu <rvishnuvishnu12@gmail.com>
2025-12-18 15:54:52 +0530

    Update SingleProduct.jsx

--- src/Shop/SingleProduct.jsx
+++ src/Shop/SingleProduct.jsx
@@ -1,97 +1,101 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import PageHeader from "../components/PageHeader";
+import productData from "../products.json"; // ✅ JSON IMPORT
 
-// Import Swiper React components
+// Swiper imports
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
-import { Autoplay } from "swiper/modules";
+import { Autoplay, Navigation } from "swiper/modules";
+
 import ProductDisplay from "./ProductDisplay";
 
 const SingleProduct = () => {
-  const [product, setProduct] = useState([]);
   const { id } = useParams();
-  //console.log (id)
-  useEffect(() => {
-    fetch("/src/products.json")
-      .then((res) => res.json())
-      .then((data) => setProduct(data));
-  }, []);
 
-  const result = product.filter((p) => p.id === id);
-  //console.log(result);
+  // Filter product from imported JSON
+  const result = productData.filter(
+    (p) => String(p.id) === String(id)
+  );
 
   return (
     <div>
       <PageHeader
-        title={"OUR  SHOP SINGLE"}
+        title={"OUR SHOP SINGLE"}
         curPage={"Shop / Single Product"}
       />
+
       <div className="shop-single padding-tb aside-bg">
         <div className="container">
           <div className="row justify-content-center">
+
+            {/* LEFT SIDE */}
             <div className="col-lg-8 col-12">
               <article>
                 <div className="product-details">
-                  <div className="row algin-items-center">
-                    <div className="col-md-6 col-12">
-                      <div className="product-thumb">
-                        <div className="swiper-container pro-single-top">
-                          <Swiper 
+                  <div className="row align-items-center">
 
-                          spaceBetween={30}
-                          slidesPerView={1}
-                          Loop={"true"}
-                          autoplay={{
-                            delay: 2000,
-                            disableOnInteraction: false
-                          }}
-                          modules={[Autoplay]}
-                          navigation ={
-                            {
-                              prevEl: ".pro-single-prev",
-                              nextElEl: ".pro-single-next",
-                            }
-                          }
-                          className="mySwiper">
-                            {
-                              result.map((item, i) =>(
-                                <swiperSilde key={i}>
-                                 <div className="single-thumb">
-                                   <img src={item.img} alt="" />
-                                 </div>
-                                </swiperSilde>
-                              ))
-                            }
-                          </Swiper>
-                          <div className="pro-single-next">
-                              <i className="icofont-rounded-left"></i>
-                          </div>
-                          <div className="pro-single-prev">
-                              <i className="icofont-rounded-right"></i>
-                          </div>
-                        </div>
+                    {/* IMAGE SLIDER */}
+                    <div className="col-md-6 col-12">
+                      <Swiper
+                        spaceBetween={30}
+                        slidesPerView={1}
+                        loop={true}                 // ✅ FIX
+                        autoplay={{
+                          delay: 2000,
+                          disableOnInteraction: false,
+                        }}
+                        navigation={{
+                          prevEl: ".pro-single-prev",
+                          nextEl: ".pro-single-next", // ✅ FIX
+                        }}
+                        modules={[Autoplay, Navigation]}
+                        className="mySwiper"
+                      >
+                        {result.map((item) => (
+                          <SwiperSlide key={item.id}>
+                            <div className="single-thumb">
+                              <img
+                                src={item.img}
+                                alt={item.name}
+                              />
+                            </div>
+                          </SwiperSlide>
+                        ))}
+                      </Swiper>
+
+                      {/* SLIDER BUTTONS */}
+                      <div className="pro-single-prev">
+                        <i className="icofont-rounded-left"></i>
+                      </div>
+                      <div className="pro-single-next">
+                        <i className="icofont-rounded-right"></i>
                       </div>
                     </div>
 
+                    {/* PRODUCT INFO */}
                     <div className="col-md-6 col-12">
-                      <div className="post-content">
-                         <div>
-                          {
-                            result.map(item => <ProductDisplay key={item.id} item={item}/>)
-                          }
-                         </div>
-                      </div>
+                      {result.map((item) => (
+                        <ProductDisplay
+                          key={item.id}
+                          item={item}
+                        />
+                      ))}
                     </div>
+
                   </div>
                 </div>
-                {/**reaview */}
+
+                {/* REVIEW SECTION */}
                 <div className="Review">Review</div>
               </article>
             </div>
-            <div className="col-lg-4 col-12">Right side</div>
+
+            {/* RIGHT SIDE */}
+            <div className="col-lg-4 col-12">
+              Right side
+            </div>
+
           </div>
         </div>
       </div>
