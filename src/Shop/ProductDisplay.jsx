1f184f8ababc79ee522f0538570818c900f5c461
vishnu <rvishnuvishnu12@gmail.com>
2025-12-18 15:26:13 +0530

    Update ProductDisplay.jsx

--- src/Shop/ProductDisplay.jsx
+++ src/Shop/ProductDisplay.jsx
@@ -1,29 +1,24 @@
 import React, { useState } from "react";
 
 const desc =
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio fuga omnis deleniti quibusdam quasi, recusandae repudiandae impedit, aspernatur fugit, molestiae mollitia inventore. Blanditiis suscipit at fugit expedita tempora, id ex.";
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio fuga omnis deleniti quibusdam quasi, recusandae repudiandae impedit, aspernatur fugit, molestiae mollitia inventore.";
 
 const ProductDisplay = ({ item }) => {
   if (!item) return null;
 
   const { name, price, seller, ratingsCount, quantity } = item;
 
-  const [preQuantity, setPreQuantity] = useState(quantity || 1);
+  const [qty, setQty] = useState(quantity || 1);
   const [size, setSize] = useState("");
   const [color, setColor] = useState("");
 
-  const increaseQty = () => {
-    setPreQuantity((prev) => prev + 1);
-  };
-
-  const decreaseQty = () => {
-    setPreQuantity((prev) => (prev > 1 ? prev - 1 : 1));
-  };
+  const increaseQty = () => setQty((p) => p + 1);
+  const decreaseQty = () => setQty((p) => (p > 1 ? p - 1 : 1));
 
   return (
     <div className="product-display">
-      {/* PRODUCT INFO */}
-      <div>
+      {/* Product Info */}
+      <div className="product-content">
         <h4>{name}</h4>
 
         <p className="rating">
@@ -35,72 +30,50 @@ const ProductDisplay = ({ item }) => {
           <span> ({ratingsCount} reviews)</span>
         </p>
 
-        <h4>${price}</h4>
+        <h4 className="price">${price}</h4>
         <h6>Seller: {seller}</h6>
         <p>{desc}</p>
       </div>
 
-      {/* CART SECTION */}
-      <div>
-        <form>
-          {/* SIZE */}
-          <div className="selected-product size">
-            <select value={size} onChange={(e) => setSize(e.target.value)}>
-              <option value="">Select Size</option>
-              <option value="SM">SM</option>
-              <option value="MD">MD</option>
-              <option value="LG">LG</option>
-              <option value="XL">XL</option>
-              <option value="XXL">XXL</option>
-            </select>
-            <i className="icofont-rounded-down"></i>
-          </div>
+      {/* Cart */}
+      <form className="cart-form">
+        {/* Size */}
+        <div className="select-box">
+          <select value={size} onChange={(e) => setSize(e.target.value)}>
+            <option value="">Select Size</option>
+            <option value="SM">SM</option>
+            <option value="MD">MD</option>
+            <option value="LG">LG</option>
+            <option value="XL">XL</option>
+            <option value="XXL">XXL</option>
+          </select>
+          <i className="icofont-rounded-down"></i>
+        </div>
 
-          {/* COLOR */}
-          <div className="selected-product color">
-            <select value={color} onChange={(e) => setColor(e.target.value)}>
-              <option value="">Select Color</option>
-              <option value="pink">Pink</option>
-              <option value="ash">Ash</option>
-              <option value="red">Red</option>
-              <option value="yellow">Yellow</option>
-              <option value="blue">Blue</option>
-            </select>
-            <i className="icofont-rounded-down"></i>
-          </div>
+        {/* Color */}
+        <div className="select-box">
+          <select value={color} onChange={(e) => setColor(e.target.value)}>
+            <option value="">Select Color</option>
+            <option value="pink">Pink</option>
+            <option value="ash">Ash</option>
+            <option value="red">Red</option>
+            <option value="yellow">Yellow</option>
+            <option value="blue">Blue</option>
+          </select>
+          <i className="icofont-rounded-down"></i>
+        </div>
 
-          {/* QUANTITY */}
-          <div className="cart-plus-minus">
-            <button
-              type="button"
-              className="dec qtybutton"
-              onClick={decreaseQty}
-            >
-              -
-            </button>
+        {/* Quantity */}
+        <div className="cart-plus-minus">
+          <button type="button" onClick={decreaseQty}>-</button>
+          <input type="text" value={qty} readOnly />
+          <button type="button" onClick={increaseQty}>+</button>
+        </div>
 
-            <input
-              className="cart-plus-minus-box"
-              type="text"
-              value={preQuantity}
-              readOnly
-            />
-
-            <button
-              type="button"
-              className="inc qtybutton"
-              onClick={increaseQty}
-            >
-              +
-            </button>
-          </div>
-
-          {/* ADD TO CART */}
-          <button type="button" className="lab-btn mt-3">
-            ADD TO CART
-          </button>
-        </form>
-      </div>
+        <button type="button" className="lab-btn">
+          ADD TO CART
+        </button>
+      </form>
     </div>
   );
 };
